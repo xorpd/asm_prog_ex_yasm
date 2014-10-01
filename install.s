4de0ff8f0485438cfb5ed8a3cf6aee8dec7b587c
blankwall <1blankwall1@gmail.com>
2014-10-01 15:06:05 -0400

    Update install.s

--- install.s
+++ install.s
@@ -1,7 +1,6 @@
 #! /bin/bash
 
 cd ..
-<<<<<<< HEAD
 wget http://www.tortall.net/projects/yasm/releases/yasm-1.3.0.tar.gz
 tar -xf yasm-1.3.0.tar.gz 
 cd yasm-1.3.0
@@ -11,12 +10,5 @@ sudo make install
 cd ..
 mkdir assembly
 cd assembly
-=======
-wget http://www.tortall.net/projects/yasm/releases/yasm-1.3.0.tar.gz ; tar -xf yasm-1.3.0.tar.gz ; cd yasm-1.3.0;
-./configure; 
-make; 
-sudo make install;
-cd ..; mkdir assembly; cd assembly;
->>>>>>> FETCH_HEAD
 cp ../asm_prog_ex/include/training.s training.s 
 cp ../asm_prog_ex/include/test.asm test.asm 
